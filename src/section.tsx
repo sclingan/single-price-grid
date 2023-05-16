69b298235e2e173eea3d48c92589f33c1cb6c8cf
Scott Clingan <sclingan1@gmail.com>
2023-05-16 14:38:43 -0500

    Added basic layout

--- src/section.tsx
+++ src/section.tsx
@@ -5,8 +5,8 @@ function Section() {
     <section>
         <h3>Monthly Subscription</h3>
 
-        <h4>$29 per month</h4>
-        <p>
+        <h4>$29 <span>per month</span></h4>
+        <p className="p-price">
         Full access for less than $1 a day
         </p>
         <a href="#">Sign Up</a>
